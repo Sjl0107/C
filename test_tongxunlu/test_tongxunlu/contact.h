b8fbb49abf47c291b020440049557502ddd055f8
Sjl0107 <52736510+Sjl0107@users.noreply.github.com>
2021-07-27 09:50:31 +0800

    Add files via upload

---
+++ test_tongxunlu/test_tongxunlu/contact.h
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#define DEFAULT_SZ 3
+#define INC 2
+
+#define MAX 1000
+#define MAX_NAME 20
+#define MAX_TEL 12
+#define MAX_ADDR 100
+#define MAX_QQ 12
+#define MAX_SEX 5
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<assert.h>
+
+//类型的声明 函数的声明
+typedef struct PeoInfo
+{
+	char name[MAX_NAME];
+	char tele[MAX_TEL];
+	char addr[MAX_ADDR];
+	char qq[MAX_QQ];
+	char sex[MAX_SEX];
+	int  age;
+}PeoInfo;
+//通讯录
+//typedef struct Contact
+//{
+//	PeoInfo data[MAX];
+//	int sz;
+//}Contact;
+//默认可以放5个人的信息
+typedef struct Contact
+{
+	PeoInfo *data;//数据
+	int sz;//有效个数
+	int capacity;//通讯录当前的容量
+}Contact;
+
+//初始化
+void InitContact(Contact* pc);
+
+//添加一个人的信息
+void add_contact(Contact* pc);
+//显示通讯录中的信息
+void show_contact(Contact* pc);
+//删除指定的联系人
+void del_contact(Contact* pc);
+//查找指定联系人
+void search_contact(Contact* pc);
+//修改
+void modify_contact(Contact* pc);
+//排序
+void sort_contact(Contact* pc);
+//销毁
+void destroy_contact(Contact* pc);
---
+++ test_tongxunlu/test_tongxunlu/test.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"contact.h"
+
+//通讯录
+//要包含人的信息：
+//名字、电话、地址、QQ、性别、年龄等
+//功能：
+//增加、删除、查找、修改、排序、退出
+
+
+void menu() {
+	printf("************************************\n");
+	printf("******  1、add     2、del     ******\n");
+	printf("******  3、search  4、modify  ******\n");
+	printf("******  5、sort    6、show    ******\n");
+	printf("******        0、exit         ******\n");
+	printf("************************************\n");
+}
+
+enum Option {
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SORT,
+	SHOW
+};
+
+void test() {
+	
+	//PeoInfo data[MAX];//存放1000个人的数据
+	//int sz = 0;//通讯录中有效的个数
+	Contact con = { 0 };
+	//初始化通讯录
+	InitContact(&con);
+
+	int input = 0;
+	do {
+		menu();
+		printf("请选择：>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			//add_contact(data,sz);
+			add_contact(&con);
+			break;
+		case DEL:
+			del_contact(&con);
+			break;
+		case SEARCH:
+			search_contact(&con);
+			break;
+		case MODIFY:
+			modify_contact(&con);
+			break;
+		case SORT:
+			sort_contact(&con);
+			break;
+		case SHOW:
+			show_contact(&con);
+			break;
+		case EXIT:
+			destroy_contact(&con);
+			printf("退出通讯录\n");
+			break;
+
+		default:
+			printf("选择错误，请重新输入\n");
+			break;
+		}
+	} while(input);
+}
+
+int main() {
+	test();
+	return 0;
+}
